bca9b23e8f3e471d4823fcdb7f100fcdd2383860
AUD-GITHUB <85228103+AUD-GITHUB@users.noreply.github.com>
2023-06-18 09:44:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -25,8 +25,8 @@ function gotPoses(results){
         console.log(results);
         console.log('nose X = '+ results[0].pose.nose.x);
         console.log('nose Y = '+ results[0].pose.nose.y);
-        noseX = results[0].pose.nose.x;
-        noseY = results[0].pose.nose.y;
+        noseX = results[0].pose.nose.x+15;
+        noseY = results[0].pose.nose.y+15;
     }
 }
 
